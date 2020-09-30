cbf14124b6eef5b5b28f7e3b9976608a5ffa7275
Maxim Samorukov <maxim.samorukov@gmail.com>
2020-09-30 22:07:03 +0300

    app: update js files

---
+++ cipher.js
@@ -0,0 +1,27 @@
+export default function (shift, text) {
+  const originalAlphabetString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const originalAlphabetArray = originalAlphabetString.split('');
+  const alphabetLength = originalAlphabetArray.length;
+  const normaliseShift = Math.abs(shift) > alphabetLength ? shift % alphabetLength : shift;
+  const firstChunk = originalAlphabetString.slice(normaliseShift);
+  const secondChunk = originalAlphabetString.slice(0, normaliseShift);
+  const cipheredAlphabetString = firstChunk + secondChunk;
+  const cipheredAlphabetArray = cipheredAlphabetString.split('');
+  const cipheredAlphabetLength = cipheredAlphabetArray.length;
+  console.log(originalAlphabetString);
+  console.log(`original alphabet length: ${alphabetLength}`);
+  console.log(`shift: ${shift}`);
+  console.log(`normaliseShift: ${normaliseShift}`);
+  console.log(cipheredAlphabetString);
+  console.log(`ciph_A_L: ${cipheredAlphabetLength}`);
+
+  const returnData = text.split('').map((i) => {
+    if (i === ' ') {
+      return ' ';
+    }
+    return cipheredAlphabetArray[originalAlphabetArray.indexOf(i)];
+  }).join('');
+  console.log(text);
+  console.log(returnData);
+  return ('cipher');
+}
--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 #!/usr/bin/env node
 import minimist from 'minimist';
+import cipher from './cipher.js';
+
+
 
 const argv = minimist(process.argv);
-console.log('Hi');
-let answer = 'False request';
-let keys = Object.keys(argv);
-console.log(keys);
-if (keys.includes('a')) {
-  answer = argv['a'];
-}
 
-console.log(answer);
+const returnObject = {
+  shift: 'N/A',
+  input: 'N/A',
+  output: 'N/A',
+  action: 'N/A'
+};
+
+const normaliseKeys = {
+  s: 'shift',
+  shift: 'shift',
+  i: 'input',
+  input: 'input',
+  o: 'output',
+  output: 'output',
+  a: 'action',
+  action: 'action',
+};
+
+const normaliseKeysArray = Object.keys(normaliseKeys);
+
+const args = {
+  shift: 'shift',
+  input: 'input file',
+  output: 'output file',
+  action: 'action',
+};
+console.log('Hi');
+let keys = Object.keys(argv);
+
+
+keys.map((i) => {
+  if (normaliseKeysArray.includes(i)) {
+    returnObject[normaliseKeys[i]] = argv[i]
+  }
+})
+const text = 'THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG';
+const cText = 'QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD'
+cipher(returnObject.shift, cText);
+// const shift = keys.includes()
