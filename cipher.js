5fa46dceaf14eb1d5c87ad1ee0d1b4bd170ffc29
Maxim Samorukov <maxim.samorukov@gmail.com>
2020-10-01 23:45:38 +0300

    app: update js files

--- cipher.js
+++ cipher.js
@@ -1,19 +1,15 @@
-export default function (shift, text) {
+export default function ({ shift, action, output, input }, text) {
+  console.log(text);
   const originalAlphabetString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   const originalAlphabetArray = originalAlphabetString.split('');
   const alphabetLength = originalAlphabetArray.length;
-  const normaliseShift = Math.abs(shift) > alphabetLength ? shift % alphabetLength : shift;
+  const normaliseShiftBeforeAction = Math.abs(shift) > alphabetLength ? shift % alphabetLength : shift;
+  const normaliseShift = action === 'encode' ? normaliseShiftBeforeAction : 26 - normaliseShiftBeforeAction;
   const firstChunk = originalAlphabetString.slice(normaliseShift);
   const secondChunk = originalAlphabetString.slice(0, normaliseShift);
   const cipheredAlphabetString = firstChunk + secondChunk;
   const cipheredAlphabetArray = cipheredAlphabetString.split('');
   const cipheredAlphabetLength = cipheredAlphabetArray.length;
-  // console.log(originalAlphabetString);
-  // console.log(`original alphabet length: ${alphabetLength}`);
-  // console.log(`shift: ${shift}`);
-  // console.log(cipheredAlphabetString);
-  // console.log(`ciph_A_L: ${cipheredAlphabetLength}`);
-
   const returnData = text.split('').map((i) => {
     if (i === ' ' || (!originalAlphabetArray.includes(i))) {
       return i;
@@ -21,8 +17,5 @@ export default function (shift, text) {
 
     return cipheredAlphabetArray[originalAlphabetArray.indexOf(i)];
   }).join('');
-  // console.log(text);
-  // console.log(returnData);
-  // console.log(text === returnData);
   return returnData;
 }
--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 #!/usr/bin/env node
 import minimist from 'minimist';
 import cipher from './cipher.js';
-import fs from 'fs';
+import fs, { Stats } from 'fs';
+import Stream from 'stream';
 
 
 
 const argv = minimist(process.argv);
 
 const returnObject = {
-  shift: 'N/A',
-  input: 'N/A',
-  output: 'N/A',
-  action: 'N/A'
+  shift: false,
+  input: false,
+  output: false,
+  action: false
 };
 
 const normaliseKeys = {
@@ -33,23 +34,110 @@ const args = {
   output: 'output file',
   action: 'action',
 };
-// console.log('Hi');
+
 let keys = Object.keys(argv);
 
 
 keys.map((i) => {
+
   if (normaliseKeysArray.includes(i)) {
-    returnObject[normaliseKeys[i]] = argv[i]
+
+    returnObject[normaliseKeys[i]] = argv[i] !== true ? argv[i] : false;
   }
+
+  return i;
 })
 const text = 'THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG';
 const cText = 'QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD';
 const text1 = 'This is secret. Message about "_" symbol!';
-const fileText = fs.readFileSync('./textNew.txt', 'utf8');
-// console.log(fileText);
+let streamFromFile;
+let streamToFile;
+let returnValue;
+let value = '';
+const { shift, action, output, input } = returnObject;
+console.log(returnObject);
 
-const newFileText = fs.readFileSync('./text.txt', 'utf8').concat('\n').concat(cipher(returnObject.shift, fileText.toUpperCase()));
-// let retData = cipher(returnObject.shift, fileText.toUpperCase());
-fs.writeFileSync('./text.txt', newFileText, 'utf8');
-// const shift = keys.includes()
-// console.log(argv);
+if (output !== false) {
+  fs.open(output, (err, fd) => {
+    if (err) {
+      console.log('Destination file. There is no file with stated address or access denied.');
+      process.exit(1);
+    } else {
+      streamFromFile = (input === false) ? process.stdin : fs.createReadStream(input, 'utf8');
+      streamToFile = (output === false) ? process.stdout : fs.createWriteStream(output, 'utf8');
+      const fromTerminal = process.stdin;
+      const toTerminal = process.stdout;
+      const toTerminalError = process.stderr;
+      // console.log(streamFromFile);
+      streamFromFile.on('error', (er) => {
+        console.log('Source file. There is no file with stated address or access denied.');
+      });
+      // streamFromFile.pipe(streamToFile);
+
+      streamToFile
+        .on('error', (er) => {
+          console.log('File to write to. There is no file with stated address or access denied.');
+        })
+        .on('pipe', () => {
+          console.log('I\'m piped');
+        })
+
+      if ((action !== 'encode' && action !== 'decode') || shift === false) {
+        const actionFlag = (action !== 'encode' && action !== 'decode');
+        const actionFlagMessage = actionFlag ? 'Action is incorrect. ' : '';
+        const shiftFlag = (shift === false);
+        const shiftFlagMessage = shiftFlag ? 'Shift value is incorrect.' : '';
+        const errorMessage = `${actionFlagMessage}${shiftFlagMessage}\n`;
+        toTerminalError.write(errorMessage);
+        process.exit(1);
+      }
+      streamFromFile.on('data', (chunk) => {
+        chunk = ((typeof chunk === 'object') ? chunk.toString() : chunk).toUpperCase();
+        console.log(chunk);
+        returnValue = cipher(returnObject, chunk);
+        console.log('returnValue');
+        console.log(returnValue);
+        streamToFile.write(returnValue);
+      });
+      // console.log(streamFromFile);
+    }
+  })
+
+} else {
+  streamFromFile = (input === false) ? process.stdin : fs.createReadStream(input, 'utf8');
+  streamToFile = (output === false) ? process.stdout : fs.createWriteStream(output, 'utf8');
+  // console.log(streamFromFile);
+  streamFromFile.on('error', (er) => {
+    console.log('Source file. There is no file with stated address or access denied.');
+  });
+  streamFromFile.pipe(streamToFile);
+
+  streamToFile
+    .on('error', (er) => {
+      console.log('File to write to. There is no file with stated address or access denied.');
+    })
+    .on('pipe', () => {
+      console.log('I\'m piped');
+    })
+
+  if ((action !== 'encode' && action !== 'decode') || shift === false) {
+    const actionFlag = (action !== 'encode' && action !== 'decode');
+    const actionFlagMessage = actionFlag ? 'Action is incorrect. ' : '';
+    const shiftFlag = (shift === false);
+    const shiftFlagMessage = shiftFlag ? 'Shift value is incorrect.' : '';
+    const errorMessage = `${actionFlagMessage}${shiftFlagMessage}\n`;
+    process.stderr.write(errorMessage);
+    process.exit(1);
+  }
+  streamFromFile.on('data', (chunk) => {
+    chunk = ((typeof chunk === 'object') ? chunk.toString() : chunk).toUpperCase();
+    console.log(chunk);
+    returnValue = cipher(returnObject, chunk);
+    console.log('returnValue');
+    console.log(returnValue);
+    streamToFile.write(returnValue);
+  });
+}
+// returnValue = cipher(returnObject, cText);
+
+
